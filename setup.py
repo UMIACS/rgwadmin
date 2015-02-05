244dc0d4679687dbd13aede5d9b25393547e3173
Liam Monahan <liam@umiacs.umd.edu>
2015-02-05 16:30:04 -0500

    update how disutils is imported

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+try:
+    from setuptools import setup
+    extra = dict(test_suite="tests.test.suite", include_package_data=True)
+except ImportError:
+    from distutils.core import setup
+    extra = {}
+
 setup(
     name = "rgwadmin",
     packages = ["rgwadmin"],
@@ -8,4 +18,5 @@ setup(
     author_email = "derek@umiacs.umd.edu",
     url = "http://www.umiacs.umd.edu/",
     keywords = ["ceph", "radosgw"],
+    **extra
 )
