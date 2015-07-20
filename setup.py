55b1e31d3448c29bf121b4c261c758f337c76b74
Liam Monahan <liam@umiacs.umd.edu>
2015-07-20 11:38:14 -0400

    In setup.py: * include our dependencies. * add maintainer info * add updated UR since this is now on github * add a long description * add licensing info * etc...

--- setup.py
+++ setup.py
@@ -9,14 +9,30 @@ except ImportError:
     from distutils.core import setup
     extra = {}
 
+long_description = \
+'''
+rgwadmin is a Python interface to the Rados Gateway Admin API.
+
+It allows for the creation, modification, and deletion of users and their
+access keys.  Usage information can also be retrieved.  This library is going
+to save you from interacting directly with a REST interface, constructing query
+strings, parsing error codes, and all that fun stuff!  It's awesome!  Enjoy!
+'''
+
+from rgwadmin import __version__
+
 setup(
-    name = "rgwadmin",
-    packages = ["rgwadmin"],
-    version = "0.2.3",
-    description = "Python Rados Gateway Admin API",
-    author = "Derek Yarnell",
-    author_email = "derek@umiacs.umd.edu",
-    url = "http://www.umiacs.umd.edu/",
-    keywords = ["ceph", "radosgw"],
+    name="rgwadmin",
+    packages=["rgwadmin"],
+    version=__version__,
+    author="Derek Yarnell",
+    author_email="derek@umiacs.umd.edu",
+    maintainer="UMIACS Staff",
+    maintainer_email="github@umiacs.umd.edu",
+    url="https://github.com/UMIACS/rgwadmin",
+    license="LGPL v2.1",
+    description="Python Rados Gateway Admin API",
+    long_description=long_description,
+    keywords=["ceph", "radosgw", "admin api"],
     **extra
 )
