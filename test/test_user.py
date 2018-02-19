b70d40ac51430f7879423a302cc23f45f9b57946
Liam Monahan <liam@liammonahan.com>
2018-02-19 11:56:36 -0500

    Added a RGWUser.exists() method.
    Utilize this method in save() to find out if we need to create the user
    before modifying the diff of what's changed since last save.
    
    Add tests as well.
    
    Fixes #19

--- test/test_user.py
+++ test/test_user.py
@@ -10,8 +10,10 @@ logging.basicConfig(level=logging.WARNING)
 
 
 class RGWUserTest(unittest.TestCase):
+
     def setUp(self):
-        rgw = rgwadmin.RGWAdmin(secure=False, verify=False, **get_environment_creds())
+        rgw = rgwadmin.RGWAdmin(secure=False, verify=False,
+                                **get_environment_creds())
         rgwadmin.RGWAdmin.set_connection(rgw)
 
     def test_create_user(self):
@@ -21,6 +23,16 @@ class RGWUserTest(unittest.TestCase):
         self.assertTrue(u.user_id == user_id and
                         u.display_name == display_name)
         u.delete()
+
+    def test_user_exists(self):
+        user_id = id_generator()
+        display_name = id_generator(25)
+        u = RGWUser.create(user_id=user_id, display_name=display_name)
+        self.assertTrue(u.exists())
+        u.delete()
+        self.assertFalse(u.exists())
+        u.save()
+        self.assertTrue(u.exists())
 
     def test_set_quota(self):
         user_id = id_generator()
