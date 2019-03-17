7075bf48c146e4efb67c53b376d9b4b54a38238d
Liam Monahan <liam@umiacs.umd.edu>
2019-03-17 15:35:59 -0400

    Use README.md as the long_description for setuptools.
    This requires creating a MANIFEST.in to include the README file in
    source distributions.
    
    Markdown support on PyPi project descriptions is fairly new, so we must
    install recent versions of setuptools to set the content type correctly
    and have it include in PKG-INFO.
    
    We now are also starting to build wheel packages for py3.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
+
+from os import path
+from io import open  # can remove after python2 support dropped.
 import ast
 import re
+
 try:
     from setuptools import setup
     extra = dict(test_suite="tests.test.suite", include_package_data=True)
@@ -10,14 +14,9 @@ except ImportError:
     from distutils.core import setup
     extra = {}
 
-long_description = '''
-rgwadmin is a Python interface to the Rados Gateway Admin API.
-
-It allows for the creation, modification, and deletion of users and their
-access keys.  Usage information can also be retrieved.  This library is going
-to save you from interacting directly with a REST interface, constructing query
-strings, parsing error codes, and all that fun stuff!  It's awesome!  Enjoy!
-'''
+BASE_DIR = path.abspath(path.dirname(__file__))
+with open(path.join(BASE_DIR, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 install_requires = [
     "requests",
@@ -25,9 +24,8 @@ install_requires = [
 ]
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-with open('rgwadmin/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open('rgwadmin/__init__.py', encoding='utf-8') as f:
+    version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
 
 
 setup(
@@ -43,6 +41,7 @@ setup(
     license="LGPL v2.1",
     description="Python Rados Gateway Admin API",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords=["ceph", "radosgw", "admin api"],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
