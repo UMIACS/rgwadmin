7ece7718bb73e34bfa11ed12e7c1a04e86f47bff
Liam Monahan <liam@umiacs.umd.edu>
2020-02-18 09:05:28 -0500

    flake8

--- setup.py
+++ setup.py
@@ -45,13 +45,13 @@ setup(
     keywords=["ceph", "radosgw", "admin api"],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-	'Programming Language :: Python :: 2',
-    	'Programming Language :: Python :: 2.7',
-    	'Programming Language :: Python :: 3',
-    	'Programming Language :: Python :: 3.2',
-    	'Programming Language :: Python :: 3.3',
-    	'Programming Language :: Python :: 3.4',
-    	'Programming Language :: Python :: 3.5',
-    	'Programming Language :: Python :: 3.6'],
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'],
     **extra
 )
